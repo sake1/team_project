6ef6e3c0d4b9455fdf826b21e7e147872a3cd590
sake1 <timothydickys@gmail.com>
2017-11-11 09:39:59 +0700

    moti

--- src/main/java/com/project/team_project/abstracts/Sensor.java
+++ src/main/java/com/project/team_project/abstracts/Sensor.java
@@ -3,5 +3,9 @@ package com.project.team_project.abstracts;
 import com.project.team_project.interfaces.Item;
 
 public abstract class Sensor implements Item{
-	
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/main/java/com/project/team_project/interfaces/Item.java
+++ src/main/java/com/project/team_project/interfaces/Item.java
@@ -1,6 +1,5 @@
 package com.project.team_project.interfaces;
 
 public interface Item {
-	public void setValue(Object value);
-	public Object getValue();
+	public void run();
 }
