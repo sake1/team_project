0b2aebc3daa929eca6ec394a4ca4a519d9917667
timotius <eka.valentino@gmail.com>
2017-11-11 09:44:26 +0700

    Add sensors and actors

--- src/main/java/com/project/team_project/actor/AirConditioner.java
+++ src/main/java/com/project/team_project/actor/AirConditioner.java
@@ -2,7 +2,7 @@ package com.project.team_project.actor;
 
 import com.project.team_project.abstracts.Actor;
 
-public class Window extends Actor {
+public class AirConditioner extends Actor{
 
 	public void run() {
 		// TODO Auto-generated method stub
